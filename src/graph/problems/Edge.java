604bab4a2cea8616b96c469638a6cded006dade5
arunvyasnarayanan <arunvyas@apple.com>
2020-02-08 18:51:23 -0800

    graph datastructure

--- src/graph/problems/Edge.java
+++ src/graph/problems/Edge.java
@@ -3,4 +3,50 @@
  */
 package graph.problems;
 public class Edge<T> {
+
+    private boolean isDirected;
+    private Vertex<T> vertex1;
+    private Vertex<T> vertex2;
+    private int weight;
+
+    public Edge(Vertex<T> vertex1, Vertex<T> vertex2) {
+        this.vertex1 = vertex1;
+        this.vertex2 = vertex2;
+    }
+
+    public Edge(boolean isDirected, Vertex<T> vertex1, Vertex<T> vertex2, int weight) {
+        this.isDirected = isDirected;
+        this.vertex1 = vertex1;
+        this.vertex2 = vertex2;
+        this.weight = weight;
+    }
+
+
+    public Edge(boolean isDirected, Vertex<T> vertex1, Vertex<T> vertex2) {
+        this.isDirected = isDirected;
+        this.vertex1 = vertex1;
+        this.vertex2 = vertex2;
+    }
+
+    public boolean isDirected() {
+        return this.isDirected;
+    }
+
+    public Vertex<T> getVertex1() {
+        return this.vertex1;
+    }
+
+    public Vertex<T> getVertex2() {
+        return this.vertex2;
+    }
+
+    public int getWeight() {
+        return this.weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Edge [isDirected=" + isDirected + ", vertex1=" + vertex1
+                + ", vertex2=" + vertex2 + ", weight=" + weight + "]";
+    }
 }
--- src/graph/problems/Vertex.java
+++ src/graph/problems/Vertex.java
@@ -1,7 +1,54 @@
 package graph.problems;
-
+import java.util.ArrayList;
+import java.util.List;
 /**
  * Created by arunvyasnarayanan on 1/25/20.
  */
 public class Vertex<T> {
+
+    private long id;
+    private T data;
+    private List<Edge<T>> edges;
+    private List<Vertex<T>> adjacentVertex;
+
+    public Vertex(long id) {
+        this.id = id;
+        edges = new ArrayList<>();
+        adjacentVertex = new ArrayList<>();
+    }
+
+    public long getId(){
+        return id;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public void addAdjacentVertex(Edge<T> edge, Vertex<T> vertex){
+        edges.add(edge);
+        adjacentVertex.add(vertex);
+    }
+
+    public List<Edge<T>> getEdges(){
+        return edges;
+    }
+
+    public List<Vertex<T>> getAdjacentVertex(){
+        return adjacentVertex;
+    }
+
+    public int getDegree(){
+        return edges.size();
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(id);
+    }
+
 }
